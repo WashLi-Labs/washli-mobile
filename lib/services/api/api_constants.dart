9a2dc6d163041b75c55b197ac80a1984f1a2bb54
Jude Maria Bevan <judeb@lolctech.com>
2026-03-23 08:03:32 +0530

    feat: point ip to vm

--- lib/services/api/api_constants.dart
+++ lib/services/api/api_constants.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 String get kBaseUrl {
-  if (kIsWeb) return 'http://localhost:8080';
-  if (Platform.isAndroid) return 'http://localhost:8080';
-  return 'http://localhost:8080';
+  if (kIsWeb) return 'http://34.169.167.67';
+  if (Platform.isAndroid) return 'http://34.169.167.67';
+  return 'http://34.169.167.67';
 }
 const String kOrderPath = '/order';
 const String kGoogleMapsApiKey = 'AIzaSyAxZ31DcQSS6Bl-zT_eV-E39M_m-XQpXF4';
