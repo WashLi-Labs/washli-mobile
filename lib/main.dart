1c474c85f909815216449d33b8f47153045fe557
Jude Maria Bevan <judeb@lolctech.com>
2026-04-03 09:01:48 +0530

    feat: initialize project structure with app icons, Android manifest permissions, and core configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Washli',
+      title: 'WashLi',
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
       theme: AppTheme.lightTheme.copyWith(
